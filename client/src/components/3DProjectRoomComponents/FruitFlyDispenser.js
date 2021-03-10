09f17d3c6fa521ec8ecb081e64bf635a3b6a8fea
Juraj Zovinec <juraj.zovinecc@gmail.com>
2021-03-10 13:29:02 +0100

    Add onClick animation of FruitFlyDispenser.

--- client/src/components/3DProjectRoomComponents/FruitFlyDispenser.js
+++ client/src/components/3DProjectRoomComponents/FruitFlyDispenser.js
@@ -1,24 +1,31 @@
-import React, {  useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useSpring, a } from '@react-spring/three';
+import FruitFlyDispenserTank from './FruitFlyDispenserTank';
 
 const FruitFlyDispenser = (props) => {
-    // Rozdelit na 4 casti
-    // -> Base, Rotor, Nadrz, Turbina
-    const url = "FruitFlyDispenser.gltf";
-    const [gltf, set] = useState(()=>null);
+    
+    const url = "FruitFlyDispenser-Base.gltf";
+    const [gltf, set] = useState(() => null);
     const [active, setActive] = useState(false);
 
     useMemo(() => new GLTFLoader().load(url, set), [url]);
-    
+
     return gltf ?
-        (<a.mesh {...props}
-            onClick={() => setActive(!active)}
-            onPointerOver={() => props.lightUpItem(true)}
-            onPointerOut={() => props.lightUpItem(false)}
-        >
-            <primitive object={gltf.scene} />
-        </a.mesh>) : null;
+        <group>
+            <mesh {...props}
+                onClick={() => setActive(!active)}
+                onPointerOver={() => props.lightUpItem(true)}
+                onPointerOut={() => props.lightUpItem(false)}
+            >
+                <primitive object={gltf.scene} />
+            </mesh>
+            <FruitFlyDispenserTank 
+                position={props.position}
+                rotation={props.rotation}
+                scale={props.scale}
+                move={active}
+            />
+        </group> : null;
 }
- 
+
 export default FruitFlyDispenser;
---
+++ client/src/components/3DProjectRoomComponents/FruitFlyDispenserTank.js
@@ -0,0 +1,53 @@
+import React, { useMemo, useState, useEffect, useRef } from 'react';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useSpring, a } from '@react-spring/three';
+
+const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        let id = setInterval(() => {
+            savedCallback.current();
+        }, delay);
+        return () => clearInterval(id);
+    }, [delay]);
+};
+
+const FruitFlyDispenserTank = (props) => {
+
+    const url = "FruiFlyDispenser-Tank.gltf";
+    const [gltf, set] = useState(() => null);
+    const [changeDirection, setChangeDirection] = useState(() => false);
+
+    useMemo(() => new GLTFLoader().load(url, set), [url]);
+
+    useInterval(() => {
+        if (props.move === true) {
+            setChangeDirection(!changeDirection);
+        }
+    }, 200);
+
+    const animationProps = useSpring({
+        position: changeDirection ? [props.position[0], props.position[1] + 0.02, props.position[2]] : [props.position[0], props.position[1], props.position[2]]
+    });
+
+    return gltf ?
+        (<a.mesh
+            rotation={props.rotation}
+            position={animationProps.position}
+            scale={props.scale}
+        >
+            <primitive object={gltf.scene} />
+        </a.mesh>) : null;
+}
+
+export default FruitFlyDispenserTank;
+
+
+
